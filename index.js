54a5937e63d537b211382f7ceedda4a34367860c
Jessica Pope <jessica.pope.co@gmail.com>
2019-12-20 11:57:07 -0700

    removes UD for class

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
   fetch('http://bagel-api-fis.herokuapp.com/bagels')
   .then(response => response.json())
   .then(result => {
-    console.log(result)
+    console.table(result)
     renderBagels(result)
   })
 
@@ -14,13 +14,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
     result.map(bagel =>{
       let li = document.createElement('li')
       li.innerText = bagel.type
-      let deleteButton = document.createElement('button')
-      deleteButton.innerText = 'delete'
-      deleteButton.addEventListener('click', ()=>{
-        // event.target.parentNode.remove()
-        updateBagel(bagel.id)
-      })
-      li.appendChild(deleteButton)
       bagelsUl.appendChild(li)
     })
   }
@@ -42,20 +35,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
     })
   }
 
-  function deleteBagel(id){
-    fetch(`http://bagel-api-fis.herokuapp.com/bagels/${id}`, {
-      method:'DELETE'
-    })
-  }
 
-  function updateBagel(id){
-    fetch(`http://bagel-api-fis.herokuapp.com/bagels/${id}`,{
-      method:'PUT',
-      headers:{
-        'Content-Type':'application/json',
-        'Accept':'application/json'
-      },
-      body:JSON.stringify({type:"yo bagel"})
-    })
-  }
+
+
+
 })
